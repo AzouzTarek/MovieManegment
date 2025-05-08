1db3d3315f2f14700bb8b6477fc190faac4c19fc
Azouz Tarek <azouztarek47@gmail.com>
2025-05-08 14:20:18 +0200

    Version_1

---
+++ Movie-Management/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO movies (title, director, release_date, genre, rating) VALUES
+                                                                      ('The Shawshank Redemption', 'Frank Darabont', '1994-09-23', 'Drama', 9.3),
+                                                                      ('The Godfather', 'Francis Ford Coppola', '1972-03-24', 'Crime', 9.2),
+                                                                      ('The Dark Knight', 'Christopher Nolan', '2008-07-18', 'Action', 9.0),
+                                                                      ('Pulp Fiction', 'Quentin Tarantino', '1994-10-14', 'Crime', 8.9),
+                                                                      ('Fight Club', 'David Fincher', '1999-10-15', 'Drama', 8.8);
